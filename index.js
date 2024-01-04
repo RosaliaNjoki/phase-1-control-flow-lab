03217b186eb5ac8ee5740875158843c021be0c6b
RosaliaNjoki <njosali0@gmail.com>
2024-01-04 16:16:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let distance;
+  
+  if (feet <= 400){
+    // distance = feet + 199;
+    return "This one is on me!";
+  }
+  else if (feet >  2000 && feet <= 2500){
+    return "I will gladly take your thirty bucks.";
+  }else 
+  {
+    return "No can do.";
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
-}
+ 
+   return city ==="NYC" ?"Ok, sounds good.": "No go.";
+  }
 
-function switchOnCharmFromTip(){
+  // destination = (city ==="NYC") ? "Ok, sounds good.": "No go.";
+  
+
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+switch (tip) {
+ case 'generous':
+  return "Thank you so much.";
+  case 'not as generous':
+    return "Thank you.";
+  default:
+    return "Bye.";
+  
+  }
 }
